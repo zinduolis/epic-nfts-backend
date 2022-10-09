5c0e1bd3bd08eac7cd01b98d4b103a773dbd087c
Your Name <you@example.com>
2022-10-09 16:12:19 +1000

    initial commit

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("I'm exsited to get my NFT contract working!");
+    }
+}
