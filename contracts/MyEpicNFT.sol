a08754d0e86311749b40b06a1a65cedefcd631ba
Your Name <you@example.com>
2022-10-13 19:55:29 +1000

    Improved randomisation and word selection

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -11,42 +11,42 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     //TODO: update lists with random words.
-    string[] firstWords = ["afforest", "aftermath", "be on cloud nine", "becalm", "blithesome", "broadsheet", "buffoonish", "caprice", "capricious", "causerie", "chivalrous", "congratulatory", "dapper", "debonaire", "devil-may-care", "emblazon", "eudaemonia", "extremum"];
-    string[] secondWords = ["exultant", "featherbrained", "felicity", "fiddle-faddle", "gabble", "gallant", "gilt", "gleeful", "halcyon", "happy-go-lucky", "heyday", "hotheaded", "indefinite quantity", "ism", "madcap", "majestic", "merry andrew", "natty", "noble-minded"];
-    string[] thirdWords = ["nuance", "phantasy", "pollyannaish", "prate", "salad days", "sappy", "snappy", "sodaPop", "spiffy", "stunner", "timberland", "timbre", "tittle-tattle", "twaddle", "vividness", "wearisome", "whimsical", "whimsy", "zippy"];
+    string[] firstWords = ["Afforest",  "Aftermath", "Becalm",  "Blithesome",  "Broadsheet",  "Buffoonish",  "Caprice",  "Capricious",  "Causerie",  "Chivalrous",  "Congratulatory",  "Dapper",  "Debonaire",  "Devil-May-Care",  "Emblazon"];
+    string[] secondWords = ["Eudaemonia",  "Extremum",  "Exultant",  "Featherbrained",  "Felicity",  "Fiddle-Faddle",  "Gabble",  "Gallant",  "Gilt",  "Gleeful",  "Halcyon",  "Happy-Go-Lucky",  "Heyday",  "Hotheaded",  "Ism",  "Madcap",  "Majestic"];
+    string[] thirdWords = ["Merry Andrew",  "Natty",  "Noble-Minded",  "Nuance",  "Phantasy",  "Pollyannaish",  "Prate",  "Sappy",  "Snappy",  "Spiffy",  "Stunner",  "Timberland",  "Timbre",  "Tittle-Tattle",  "Twaddle",  "Vividness",  "Wearisome",  "Whimsical",  "Whimsy",  "Zippy"];
 
     constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("I'm excited to get my NFT contract working!");
     }
 
     function concatRandomWords() public view returns (string memory) {
-        uint firstWordNr = uint(keccak256(abi.encodePacked(firstWords[0], Strings.toString(_tokenIds.current())))) % firstWords.length; 
-        uint secondWordNr = uint(keccak256(abi.encodePacked(secondWords[0], Strings.toString(_tokenIds.current())))) % secondWords.length; 
-        uint thirdWordNr = uint(keccak256(abi.encodePacked(thirdWords[0], Strings.toString(_tokenIds.current())))) % thirdWords.length;
+        uint firstWordNr = uint(keccak256(abi.encodePacked(firstWords[0], Strings.toString(_tokenIds.current()), block.timestamp))) % firstWords.length; 
+        uint secondWordNr = uint(keccak256(abi.encodePacked(secondWords[0], Strings.toString(_tokenIds.current()), block.timestamp))) % secondWords.length; 
+        uint thirdWordNr = uint(keccak256(abi.encodePacked(thirdWords[0], Strings.toString(_tokenIds.current()), block.timestamp))) % thirdWords.length;
         console.log("Word numbers: %d %d %d", firstWordNr, secondWordNr, thirdWordNr); 
         string memory concatWords = string.concat(firstWords[firstWordNr], secondWords[secondWordNr], thirdWords[thirdWordNr]);
         console.log("Concatenated string is: %s", concatWords);
-        console.log("First Length: %s \n Second: %s \n Third: %s", firstWords.length, secondWords.length, thirdWords.length);
+        // console.log("First Length: %s \n Second: %s \n Third: %s", firstWords.length, secondWords.length, thirdWords.length);
         return concatWords;
     }
 
-    function createImageString(string memory _randomWords) public view returns (string memory) {
+    function createImageString(string memory _randomWords) public pure returns (string memory) {
         string memory svg = string.concat("<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>", _randomWords,"</text></svg>");
-        console.log("svg: %s", svg);
+        // console.log("svg: %s", svg);
         string memory encodedSvg = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        console.log ("encodedSvg: ", encodedSvg);
+        // console.log ("encodedSvg: ", encodedSvg);
         string memory imageString = string.concat("data:application/json;base64,",encodedSvg);
-        console.log("imageString: ", imageString);
+        // console.log("imageString: ", imageString);
         return imageString;
     }
 
-    function prepareEncodedJSON(string memory _randomWords, string memory _imageString) public view returns (string memory) {
+    function prepareEncodedJSON(string memory _randomWords, string memory _imageString) public pure returns (string memory) {
         string memory json = string.concat('{"name": "', _randomWords, '", "description" : "Funny collection of squares.", "image" : "', _imageString, '"}');
-        console.log("JSON: %s", json);
+        // console.log("JSON: %s", json);
         string memory encodedJSON = Base64.encode(bytes(string(abi.encodePacked(json))));
-        console.log("encodedJSON: %s", encodedJSON);
+        // console.log("encodedJSON: %s", encodedJSON);
         string memory jsonString = string.concat("data:application/json;base64,",encodedJSON);
-        console.log("jsonString: %s", jsonString);
+        // console.log("jsonString: %s", jsonString);
         return jsonString;
     }
 
@@ -59,6 +59,7 @@ contract MyEpicNFT is ERC721URIStorage {
         _setTokenURI(newItemId, encodedJSON);
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
         _tokenIds.increment();
+        // console.log("Block difficulty: %s, Block timestamp: %s", block.difficulty, block.timestamp);
     }
 }
 
